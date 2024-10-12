63738650b7d1f4d9ada467fdbaa0b1386e9ee7a5
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-12 18:08:07 +0200

    fix format

--- k8s-infra/subnet/variable.tf
+++ k8s-infra/subnet/variable.tf
@@ -8,12 +8,6 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "vpc_cidr" {
-  description = "AWS VPC's CIDR value"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
 variable "environment" {
   description = "Environment name"
   type        = string
